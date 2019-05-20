36f29bd4fb923b3571fb697cf4626f4c178f55ec
OSekmen <46937646+OSekmen@users.noreply.github.com>
2019-05-20 17:58:56 +0200

    Randdetectie
    Randdetectie added

---
+++ randDetectie/randDetectie.ino
@@ -0,0 +1,74 @@
+#include <VL53L0X.h>
+#include <Wire.h>
+
+VL53L0X sensorL;
+VL53L0X sensorR;
+long int distanceL;
+long int distanceR;
+long int distance;
+long int left;
+long int right;
+int sensorLreset = 8;
+int sensorRreset = 7;
+int pwm;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Wire.begin();
+
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  //reset all sensors by pulling shutdownpins low and high (HIGH = active)
+  digitalWrite(sensorLreset, LOW);
+  digitalWrite(sensorRreset, LOW);
+  delay(10);
+  digitalWrite(sensorLreset, HIGH);
+  digitalWrite(sensorRreset, HIGH);
+  delay(10);
+
+  //keep Left sensor on by pulling Right LOW, assign new adress
+  digitalWrite(sensorRreset, LOW);
+  sensorL.init();
+  sensorL.setAddress(0x30);
+  sensorL.setTimeout(500);
+
+  //repeat on Right Sensor
+  digitalWrite(sensorRreset, HIGH);
+  sensorR.init();
+  sensorR.setAddress(0x31);
+  sensorR.setTimeout(0);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  distanceL = sensorL.readRangeSingleMillimeters();
+  distanceR = sensorR.readRangeSingleMillimeters();
+  dsitanace = abs(75-distanceL)+abs(75-distanceR)/2
+  if (distanceL > 75) {
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+  } else if (distanceR > 75) {
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+  }else if(distanceL > 70 && distanceR < 80){
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+  }
+
+    Serial.print("distanceL: ");
+    Serial.print(distanceL);
+    Serial.print("  distanceR: ");
+    Serial.println(distanceR);
+    /*
+    Serial.print("75 -distance: ");
+    Serial.print(75 - distance);
+    Serial.print("  PWM: ");
+    Serial.println(pwm);
+*/
+}
