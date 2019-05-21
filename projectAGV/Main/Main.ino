4f0d70930e01610e362e65a4834ea7366d633369
Pim Stoopman <pimstoopman@gmail.com>
2019-05-21 13:42:29 +0200

    Project is aangemaakt waar alle codes in toegevoegd kunnen worden. In Visual Studio 2017

---
+++ projectAGV/Main/Main.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		Main.ino
+ Created:	5/21/2019 1:37:04 PM
+ Author:	Pim Stoopman
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+  
+}
