02e09376b05a03019f948e329524aec0f2c13721
Bas Ruigrok <18003982@student.hhs.nl>
2019-06-07 13:42:11 +0200

    Ik weet het niet meer

---
+++ Global/Pins.h
@@ -0,0 +1,40 @@
+#pragma once
+/*
+In dit bestand staan welke pins gebruikt moeten worden
+*/
+
+#pragma region Obstakel Detectie
+
+#define FRONT_TRIGGER 0
+#define BACK_TRIGGER 0
+#define FRONT_ECHO 0
+#define BACK_ECHO 0
+
+#define SERVO 0
+
+#pragma endregion
+
+#pragma region ToF Detectie
+
+#define RESET_RAND_L (1<<1)
+#define RESET_RAND_R (1<<2)
+#define RESET_BOOM_L (1<<3)
+#define RESET_BOOM_R (1<<4)
+
+#pragma endregion
+
+#pragma region Aandrijving
+
+#define DIRECTION_L 0
+#define PULSE_L		0
+#define DIRECTION_R 0
+#define PULSE_R		0
+
+#pragma endregion
+
+#pragma region IC control
+#define LATCHPIN 0
+#define CLOCKPIN 0
+#define DATAPIN	 0
+static uint8_t IC_REG = 0;
+#pragma endregion
---
+++ Navigatie/Navigatie.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Pins.h"
+#include "GlobalVariables.h"
+
+void setupNavigatie() {
+	orientation = Orientation::POSITIVE_Y;
+}
+
+void loopNavigatie() {
+
+}
